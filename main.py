67df5c6bdcf01bcdeb1aafe2dc661a4f1137d180
Sierra Strickland <sstrickland@cub.uca.edu>
2023-05-05 10:52:18 -0500

    Added a bunch of changes. Add a function that reads the file. Having it to where the x and y values are being added to the appropriate values. Added some error statements. Not getting a graph as the ValueEror statement is being called. Will try and fix this. As well as add comments.

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-# import the library that will give us the tools to plot the data
-from pylab import *
+# importing libraries
+import matplotlib.pyplot as plt
 
-data = np.loadtxt('gasData.dat')
+try:
+    with open('./gasData.dat', 'r') as file:
+        data = file.readlines()
+except FileNotFoundError:
+    print("File not found")
+    exit()
 
-# x values
-x = [1 ,2, 3]
+xData = []
+yData = []
 
-# y values
-y = [1, 2, 3]
+for line in data:
+    try:
+        x, y = line.split()
+        xData.append(x)
+        yData.append(float(y))
+    except ValueError:
+        print(f"Skipping line: {line}")
+        continue
 
-plot(x, data)
-savefig('data.png')
+print(f"xData: {xData}")
+print(f"yData: {yData}")
+
+plt.plot(xData, yData)
+plt.xlabel('X label')
+plt.ylabel('Y label')
+plt.title('Title')
+plt.show()
